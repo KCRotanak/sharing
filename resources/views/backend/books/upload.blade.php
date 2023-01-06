04094cafe4dcca24a8ca9a99debb56b56d39f174
unknown <somnearkith097@gmail.com>
2023-01-06 10:45:50 +0700

    updated upload

--- resources/views/backend/books/upload.blade.php
+++ resources/views/backend/books/upload.blade.php
@@ -4,6 +4,22 @@
         <div class="main-content">
             <div class="page-content">
                 <div class="container-fluid">
+                     <!-- start page title -->
+                     <div class="row">
+                        <div class="col-12">
+                            <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                                <h4 class="mb-sm-0">book</h4>
+                                <div class="page-title-right">
+                                    <ol class="breadcrumb m-0">
+                                        <li class="breadcrumb-item">Pages</li>
+                                        <li class="breadcrumb-item active">book</li>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end page title -->
+                    
                     <div class="row">
                         <div class="col-lg-8">
                             <div class="card">
